728433e595dfa4b243c0bfbaf7baa4776d9ff58c
arthur02b <arthurbrandao700@gmail.com>
2025-06-24 11:25:04 -0300

    alteração para render

--- conexao.php
+++ conexao.php
@@ -1,4 +1,19 @@
 <?php
+$host = getenv("DB_HOST");
+$db   = getenv("DB_NAME");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASSWORD");
+
+try {
+  $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+  $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+  die("Erro na conexão: " . $e->getMessage());
+}
+
+
+// Caso não esteja usando variáveis de ambiente, descomentar a seção abaixo e comentar a seção acima
+/*
 $host = "localhost";
 $db   = "ktask";
 $user = "root";
@@ -10,4 +25,5 @@ try {
 } catch (PDOException $e) {
   die("Erro na conexão: " . $e->getMessage());
 }
+*/
 ?>
