8001d60ddad251eea3fdcb47feb6faa913601093
arthur02b <arthurbrandao700@gmail.com>
2025-06-24 11:35:53 -0300

    adaptação banco render

--- conexao.php
+++ conexao.php
@@ -1,11 +1,12 @@
 <?php
 $host = getenv("DB_HOST");
+$port = getenv("DB_PORT") ?: 5432;
 $db   = getenv("DB_NAME");
 $user = getenv("DB_USER");
 $pass = getenv("DB_PASSWORD");
 
 try {
-  $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+  $pdo = new PDO("pgsql:host=$host;port=$port;dbname=$db", $user, $pass);
   $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
   die("Erro na conexão: " . $e->getMessage());
